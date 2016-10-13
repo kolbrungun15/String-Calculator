7b09af3ca517667aaf1fc028d2e6d430252dc9b8
Kolbrún <kolbrungun15@ru.is>
2016-10-13 14:20:48 +0000

    útfærði big numbers, \n og smá af negative (þarf að laga það og bæta við)

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,6 +2,54 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 	public static int add(String text){
-		return 0;
+
+		if(text.equals(""))
+			return 0;
+
+		if(text.contains("\n"))
+			text = text.replace("\n" , ",");
+
+		throwExceptionIfNegative(text);
+		text = removeBigNumbers(text);
+		
+		return sum(splitText(text));
+	}
+
+	private static String[] splitText(String text){
+	    return text.split(",");
+	}
+
+	private static int sum(String[] text){
+ 	    int total = 0;
+        for(String number : text){
+		    total += toInt(number);
+		}
+		return total;
+    }
+
+    private static int toInt(String number){
+		return Integer.parseInt(number);
+	}
+
+	//Þarf að útfæra að hann sýni hvaða tölur eru negatívar
+	private static void throwExceptionIfNegative(String text) {
+    String[] negatives = splitText(text);
+    if (text.contains("-")) {
+        throw new IllegalArgumentException("Negative numbers not allowed: "); 
+        //+ extractNegativeNumbers(negatives)
+    	}
+
+    }
+
+    private static String removeBigNumbers(String text){
+
+		StringBuilder newnumber = new StringBuilder();
+		String[] oldnumber = splitText(text);
+		for(String numb : oldnumber){
+		    if (toInt(numb) < 1000){
+		    	newnumber.append(numb);
+		    	newnumber.append(",");}
+			}
+		return newnumber.toString();
 	}
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -10,6 +10,40 @@ public class CalculatorTest {
 
 	@Test 
 	public void testOneNumber(){
-		assertEquals(0, Calculator.add("1"));
+		assertEquals(1, Calculator.add("1"));
 	}
+
+	@Test
+	public void testTwoNumbers(){
+		assertEquals(3, Calculator.add("1,2"));
+	}
+
+	@Test
+	public void testFivrNumbers(){
+		assertEquals(13, Calculator.add("1,2,5,2,3"));
+	}
+
+	@Test
+	public void testNewLine(){
+		assertEquals(6, Calculator.add("1\n2,3"));
+	}
+
+	@Test
+	public void testNewLine2(){
+		assertEquals(11, Calculator.add("1\n2,3\n5"));
+	}
+	//Skoða betur ... ekki viss hvort þetta sé rétt.
+	@Test
+	public void testNegativeNumbers() {
+	  try {
+	    new String("-1,2");
+	  } catch (IllegalArgumentException e) {
+	  }
+	}
+
+	@Test
+	public void RemoveBigNumbers(){
+		assertEquals(2, Calculator.add("1002,2"));
+	}
+
 }
